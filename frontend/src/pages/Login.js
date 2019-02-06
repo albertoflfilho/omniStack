73e4d075e0ff0344541272f2e0ad0a44c8c0c672
Alberto Lacerda <albertoflfilho@gmail.com>
2019-02-06 15:37:23 -0500

    handleSubmit handleInputChane localStorage.setItem

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,42 @@
 import React, { Component } from 'react';
 
+import twitterLogo from '../twitter.svg';
+import './Login.css';
+
 export default class Login extends Component {
+  state = {
+    username: '',
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { username } = this.state;
+
+    if (!username.length) return;
+
+    localStorage.setItem('@TwitterAFL', username);
+
+    this.props.history.push('/timeline');
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ username: e.target.value });
+  };
+
   render() {
-    return <h1>login</h1>;
+    return (
+      <div className="login-wrapper">
+        <img src={twitterLogo} alt="twitterLogo" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.username}
+            onChange={this.handleInputChange}
+            placeholder="username"
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
   }
 }
--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class Timeline extends Component {
   render() {
-    return <h1>timeline</h1>;
+    return <h1>Timeline</h1>;
   }
 }
