1dec017fa49d72e9a47d9ffaf06e9b66b1fc07f3
Alberto Lacerda <albertoflfilho@gmail.com>
2019-02-06 13:14:49 -0500

    api endpoint with socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect(
   'mongodb://backend:backend123@ds117539.mlab.com:17539/backend',
@@ -10,9 +14,16 @@ mongoose.connect(
   }
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
+app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log(' -> Server started on port 3000');
 });
