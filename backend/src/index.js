3657215b58eb2811cb736d89ad9d8679602e99ae
Alberto Lacerda <albertoflfilho@gmail.com>
2019-02-06 10:15:04 -0500

    router created

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,9 +10,7 @@ mongoose.connect(
   }
 );
 
-app.get('/', (req, res) => {
-  return res.send('Node Express here!');
-});
+app.use(require('./routes'));
 
 app.listen(3000, () => {
   console.log(' -> Server started on port 3000');
