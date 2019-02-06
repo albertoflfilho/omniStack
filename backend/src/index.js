c311a5272119807ace77b568eeb4d05fbc1af9a3
Alberto Lacerda <albertoflfilho@gmail.com>
2019-02-06 10:03:24 -0500

    mlab conection schema model inclued

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(
+  'mongodb://backend:backend123@ds117539.mlab.com:17539/backend',
+  {
+    useNewUrlParser: true,
+  }
+);
 
 app.get('/', (req, res) => {
   return res.send('Node Express here!');
 });
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log(' -> Server started on port 3000');
 });
