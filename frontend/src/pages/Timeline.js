80735a4d5b7df4f59efc8ad2a1739e2399b55d93
Alberto Lacerda <albertoflfilho@gmail.com>
2019-02-06 15:55:05 -0500

    timeline handleNewTweet handleInputChange localStore.getItem()

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
 
+import twitterLogo from '../twitter.svg';
+import './Timeline.css';
+
 export default class Timeline extends Component {
+  state = {
+    newTweet: '',
+  };
+
+  handleNewTweet = (e) => {
+    if (e.keyCode !== 13) return;
+
+    const content = this.state.newTweet;
+    const author = localStorage.getItem('@TwitterAFL');
+
+    console.log(content, author);
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ newTweet: e.target.value });
+  };
+
   render() {
-    return <h1>Timeline</h1>;
+    return (
+      <div className="timeline-wrapper">
+        <img height={24} src={twitterLogo} alt="twitterLogo" />
+
+        <form>
+          <textarea
+            value={this.newTweet}
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleNewTweet}
+            placeholde="What's up?"
+          />
+        </form>
+      </div>
+    );
   }
 }
