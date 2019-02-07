a8d61ec7dfe75b85d90245b7d6e4e453610d07d0
Alberto Lacerda <albertoflfilho@gmail.com>
2019-02-07 15:34:34 -0500

    react-native-vector-icons AND react-native link react-native-vector-icons AND routes Login created

---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+import {
+  KeyboardAvoidView,
+  View,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+
+// import styles from './styles';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <KeyboardAvoidView behavior="padding" style={styles.container}>
+        <View style={styles.content} />
+      </KeyboardAvoidView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    height: 44,
+    paddingHorizontal: 15,
+    alignSelf: 'stretch',
+    marginTop: 30,
+  },
+
+  button: {
+    height: 44,
+    alignSelf: 'stretch',
+    marginTop: 10,
+    backgroundColor: '#4BB0EE',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
---
+++ mobile/src/pages/Timeline.js
@@ -0,0 +1,6 @@
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  }
+});
