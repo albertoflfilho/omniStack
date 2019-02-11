c6fa73fcf21b26fc5220f492f6bf2ede3e812a4d
Alberto Lacerda <albertoflfilho@gmail.com>
2019-02-11 18:07:01 -0500

    removed yellowBox

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings(['Unrecognized WebSocket']);
+YellowBox.ignoreWarnings(['Possible Unhandled']);
 
 import Routes from './routes';
 
